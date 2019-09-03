cf2685c1ee694f8708a571d1bd5109cb153aabb1
Hessah95 <hessah.alhamadi.95@gmail.com>
2019-09-03 15:22:04 +0300

    Hessah

--- flights/views.py
+++ flights/views.py
@@ -1,8 +1,20 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView, RetrieveUpdateAPIView, DestroyAPIView
+from rest_framework.generics import (
+	ListAPIView, RetrieveAPIView, 
+	RetrieveUpdateAPIView, DestroyAPIView, 
+	CreateAPIView,
+	)
 from datetime import datetime
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
+from rest_framework.views import APIView
+
 
 from .models import Flight, Booking
-from .serializers import FlightSerializer, BookingSerializer, BookingDetailsSerializer, UpdateBookingSerializer
+from .serializers import (
+	FlightSerializer, BookingSerializer, 
+	BookingDetailsSerializer, UpdateBookingSerializer,
+	UserLoginSerializer, CreateBookingSerializer,
+	)
 
 
 class FlightsList(ListAPIView):
@@ -33,3 +45,21 @@ class CancelBooking(DestroyAPIView):
 	queryset = Booking.objects.all()
 	lookup_field = 'id'
 	lookup_url_kwarg = 'booking_id'
+
+class UserLoginAPIView(APIView):
+    serializer_class = UserLoginSerializer
+
+    def post(self, request):
+        my_data = request.data
+        serializer = UserLoginSerializer(data=my_data)
+        if serializer.is_valid(raise_exception=True):
+            valid_data = serializer.data
+            return Response(valid_data, status=HTTP_200_OK)
+        return Response(serializer.errors, HTTP_400_BAD_REQUEST)
+
+
+class CreateBooking (CreateAPIView) :
+	serializer_class = UpdateBookingSerializer
+
+	def perform_create (self, serializer):
+		serializer.save(flight_id = self.kwargs['flight_id'], user = self.request.user)
